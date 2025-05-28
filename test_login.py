8334acdf108ba0170c2908acb173eae4819a8229
Atkari Ariella <atkari.ariella@diak.szbi-pg.hu>
2025-05-28 10:29:57 +0200

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,3 +1,6 @@
+#new terminalba:
+#   pip install webdriver-manager selenium
+#   python test_login.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
